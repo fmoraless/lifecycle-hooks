ebb309208e4110c10fa3b97cb51b743024907dc8
fmoraless <franticzero@gmail.com>
2026-07-05 14:04:55 -0400

    feat: implement lifecycle hooks with logging in HomePage component

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,9 +1,80 @@
-import { Component } from "@angular/core";
+import { afterNextRender, Component, effect } from '@angular/core';
 
+const log = (...messages: string[]) => {
+  console.log(
+    `${messages[0]} %c${messages.slice(1).join(', ')}`,
+    'color: #bada55; font-weight: bold;',
+  );
+};
 @Component({
-    selector: "app-home-page",
-    imports:[],
-    templateUrl: "./home-page.html",
-    
+  selector: 'app-home-page',
+  imports: [],
+  templateUrl: './home-page.html',
 })
-export class HomePage {}
+export class HomePage {
+  constructor() {
+    log('constructor llamado');
+  }
+
+  basicEffect = effect((onCleanup) => {
+    log(
+      'basicEffect llamado',
+      "Runs every time the component's state changes. It's a good place to put logic that depends on reactive state.",
+    );
+
+    onCleanup(() => {
+      log(
+        'basicEffect cleanup llamado',
+        "Runs when the component is destroyed or when the effect is re-run. It's a good place to put cleanup logic.",
+      );
+    });
+  });
+
+  ngOnInit() {
+    log(
+      'ngOnInit llamado',
+      "Runs once the component is initialized. It's a good place to put initialization logic.",
+    );
+  }
+
+  ngOnChanges() {
+    log('ngOnChanges llamado', "Runs every time the component's inputs have changed.");
+  }
+
+  ngDoCheck() {
+    log('ngDoCheck llamado', 'Runs every time the component is checked for changes.');
+  }
+
+  ngAfterContentInit() {
+    log(
+      'ngAfterContentInit llamado',
+      "Runs once after the component's content has been initialized.",
+    );
+  }
+
+  ngAfterContentChecked() {
+    log(
+      'ngAfterContentChecked llamado',
+      "Runs every time the component's content has been checked.",
+    );
+  }
+
+  ngAfterViewInit() {
+    log('ngAfterViewInit llamado', "Runs once after the component's view has been initialized.");
+  }
+
+  ngAfterViewChecked() {
+    log('ngAfterViewChecked llamado', "Runs every time the component's view has been checked.");
+  }
+
+  ngOnDestroy() {
+    log('ngOnDestroy llamado', 'Runs once the component is about to be destroyed.');
+  }
+
+  afterNextRenderEffect = afterNextRender(() => {
+    log(
+      'afterNextRenderEffect llamado',
+      "Runs after the next render cycle. It's a good place to put logic that depends on the DOM being updated.",
+    );
+  });
+}
